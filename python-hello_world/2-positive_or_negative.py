9d5df4604a33830a44bb7aa167862db4d36a2a4e
youneskoussour <younesksr99@gmail.com>
2023-11-27 22:10:08 +0100

    commit positive

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -4,9 +4,9 @@ number = random.randint(-10, 10)
 print("Please enter a number between -10 and +10",number)
 if number>0:
     print("The number is positive.")
-elif number == 0:
+if number == 0:
     print("The number is zero.")
-else:
+if number<0:
     print("The number is negative.")
 
 
