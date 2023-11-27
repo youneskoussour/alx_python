e21aaab32073b3c91496a2d9532effb81023fe1d
youneskoussour <younesksr99@gmail.com>
2023-11-27 22:32:53 +0100

    commit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python3s
 import random
 number = random.randint(-10, 10)
-print("Please enter a number between -10 and +10",number)
-if number>0:
-    print(f'{number} is positive.')
-elif  number == 0:
-    print(f'{number} is zero.')
+
+
+if  number  > 0:
+    print(number, "is positive")
+    
+elif number == 0:
+        print(number,"is zero")
+
 else:
-    print(f'{number} is negative.')
-
-
+    print(number, "is negative")
 
