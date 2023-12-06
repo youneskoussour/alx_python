05c5e60d35688400a736715df500cc433642fead
youneskoussour <younesksr99@gmail.com>
2023-12-06 21:27:46 +0100

    commit squar matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = matrix[i][j] ** 2
+    return new_matrix
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+about more data structure
