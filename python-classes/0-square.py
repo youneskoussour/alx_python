4bccd979ad9746b1e488dc3b81ce9c9a3a61a543
youneskoussour <younesksr99@gmail.com>
2023-12-14 22:08:43 +0100

    second commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,6 @@
+"""This is a class square that defines a square."""
 class Square:
+    """This class represent square of sizes"""
     def __init__(self, size):
+        """size is a private instance attributes"""
         self.__size = size
