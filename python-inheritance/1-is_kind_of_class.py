2ee25737a19becab8ca1dfbf60f798b653d46042
youneskoussour <younesksr99@gmail.com>
2023-12-14 22:40:54 +0100

    commit kind of class

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,6 @@
+"""This is an instance of the same class or inherit"""
+
+def is_kind_of_class(obj, a_class):
+
+    """Return True if object is an instance of the class"""
+    return isinstance(obj, a_class)
