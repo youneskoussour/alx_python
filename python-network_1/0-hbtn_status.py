ec79f6b30e828b7c76f9b98b464a3a4028e61d26
youneskoussour <younesksr99@gmail.com>
2023-12-21 21:19:52 +0100

    commit first

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,3 +3,5 @@ import requests
 
 req = 'https://alu-intranet.hbtn.io/status'
 response = requests.get(req)
+
+print("Body response:\n\t- type: {}\n\t- content: {}".format(type(response.text), response.text))
