d7fb7c5e3043162d8877b00770c1a5cea1cfe985
youneskoussour <younesksr99@gmail.com>
2024-03-22 14:59:33 +0000

    commit

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -1,45 +1,33 @@
-"""
-Import flask
-"""
+'''import flask server'''
 from flask import Flask
 app = Flask(__name__)
 
+        
+@app.route("/",strict_slashes=False)
 
-@app.route('/', strict_slashes=False)
 def Hello_HBNB():
-    """created my first route"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
+@app.route("/hbnb")
+def HBNB():
+    return "HBNB"
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """added a new route that display something else"""
-    return 'HBNB'
+@app.route("/c/<text>")
+def khadija(text):
+    formated_text = text.replace("_", " ")
+    return "C {}".format(formated_text)
 
+    #return f"C {formated_text}" 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_way(text):
-    """display C then followed by any value appeneded to it"""
-    return 'C ' + text.replace('_', ' ')
+@app.route("/python/<text>")
+@app.route("/python/")
+def python_text(text = "is cool"):
+    formated_text = text.replace("_", " ")
+    return "Python {}".format(formated_text)
 
-
-@app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    """
-    declare two route that has a default string
-    but can change when needed
-    """
-    return 'Python ' + text.replace('_', ' ')
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def int_validator(n):
-    """
-    detects if the entered argument is a number.
-    if not return a error page
-    """
-    return '{:d} is a number'.format(n)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+@app.route("/number/<int:n>")
+def python_integer(n):
+    return "{} is a number".format(n)
+    
+if __name__=="__main__":
+    app.run(host='0.0.0.0',port=5000, debug=True)
