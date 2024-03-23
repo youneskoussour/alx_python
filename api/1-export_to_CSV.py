c649861abcd5b09e670182e63ffd4fb0cb0d5a9e
youneskoussour <younesksr99@gmail.com>
2024-03-23 17:39:36 +0000

    com

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,47 +2,20 @@ import csv
 import requests
 import sys
 
-def fetch_todo_list_progress(employee_id):
-    # Fetching employee details
-    employee_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    employee_response.raise_for_status()  # Check if the request was successful
-    employee_data = employee_response.json()
-    employee_name = employee_data['name']
+user_id = str(sys.argv[1])
 
-    # Fetching todo list for the employee
-    todo_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
-    todo_response.raise_for_status()  # Check if the request was successful
-    todo_data = todo_response.json()
+request_user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+request_todos = "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)
 
-    # Writting data to CSV file
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for task in todo_data:
-            writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
+data_user = requests.get(request_user).json()
+data_todos = requests.get(request_todos).json()
 
-    print(f"Data exported to {csv_filename}")
+filename = f"{user_id}.csv"
 
-    # Return the number of tasks in the CSV file
-    return len(todo_data)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-    employee_id = sys.argv[1]
-    try:
-        num_tasks = fetch_todo_list_progress(employee_id)
-        print(f"Number of tasks in CSV: OK")
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
-    except KeyError:
-        print("Error: Employee data not found.")
-        sys.exit(1)
-    except FileNotFoundError:
-        print(f"Number of tasks in CSV: OK")
-        # sys.exit(1)  # Uncomment this line if you want the script to exit with an error
+with open(filename, "w", newline="") as file:
+    csvwriter = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for task in data_todos:
+        csvwriter.writerow(
+            [user_id, str(data_user["username"]), task["completed"], task["title"]]
+        )
+        
