fbdca8c0e738a952e5fadd07d73a1d4003eb9ebc
youneskoussour <younesksr99@gmail.com>
2024-03-25 11:02:48 +0000

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,5 +1,13 @@
-if __name__ == '__main__':
-    from args import myarguments
-    result = myarguments()
-    if result:
-        print(result)
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]  
+
+    num_arguments = len(argv)
+
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print(f"{num_arguments} argument{'s' if num_arguments > 1 else ''}:")
+        for i, arg in enumerate(argv, 1):
+            print(f"{i}: {arg}")
